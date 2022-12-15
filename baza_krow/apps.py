ae6ef936f88908d777c9b7d34e257bf2a32d733d
Robert Swierc <robert@MacBook-Pro-Robert.local>
2022-12-15 18:42:40 +0100

    Deleted files

--- baza_krow/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BazaKrowConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'baza_krow'
--- baza_krow/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py in new application
-
-from django.urls import path
-from . import views #import views from current folder
-
-urlpatterns = [
-    path("", views.index, name="index"), # path to new view
-]

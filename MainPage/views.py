58ffaaf8785e3eebe01b5084c66d2646cef3975b
Robert Swierc <robert@MacBook-Pro-Robert.local>
2022-12-19 21:19:23 +0100

    Finished weather API and links

--- MainPage/views.py
+++ MainPage/views.py
@@ -3,20 +3,27 @@ from django.shortcuts import render
 
 # Create your views here
 def weather(request):
-    url = 'https://api.openweathermap.org/data/2.5/weather?q=Wrocław&APPID=8ba66185083e4093fce75a96ace553fc'
-    city = 'Wroclaw'
+    city = ['Wrocław', 'Opole', 'Kobylno', 'Bierdzany']
+    r = []
+    weather = []
+    for count, i in enumerate(city):
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={i}&APPID=8ba66185083e4093fce75a96ace553fc'
+        r.append(requests.get(url.format(i)).json())
 
-    r = requests.get(url.format(city)).json()
-    
-    city_weather = {
-        'city' : city,
-        'temperature' : r['main']['temp'],
-        'feels_like' : r['main']['feels_like'],
-        'temp_min' : r['main']['temp_min'],
-        'temp_max' : r['main']['temp_max'],
-        'icon' : r['weather'][0]['icon'],
-        'clouds' : r['clouds']['all'],
-    }
+        weather_city = {
+            'city' : i,
+            'temperature' : round(int(r[count]['main']['temp'])-273.15, 1),
+            'feels_like' : round(int(r[count]['main']['feels_like'])-273.15, 1),
+            'temp_min' : r[count]['main']['temp_min'],
+            'temp_max' : r[count]['main']['temp_max'],
+            'icon' : r[count]['weather'][0]['icon'],
+            'clouds' : r[count]['clouds']['all'],
+            'wind' : r[count]['wind']['speed'],
+        }
+       
+        weather.append(weather_city)
 
-    
-    return render(request, "MainPage/home_page.html", city_weather)
+
+ 
+    context = {'weather': weather}
+    return render(request, "MainPage/home_page.html", context)
